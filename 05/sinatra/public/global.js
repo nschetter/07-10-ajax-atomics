4d204bb2884fdf70d30eccf10656b31c99501a78
Nick <nschette@yahoo.com>
2015-07-13 11:32:26 -0500

    last atomic

--- 05/sinatra/public/global.js
+++ 05/sinatra/public/global.js
@@ -1,5 +1,25 @@
 var finishLinks = document.getElementsByClassName("done_link");
 
 for (var i=0; i < finishLinks.length; i++) {
-  // finishLinks[i] represents each of the items in `finishLinks`.
+  finishLinks[i].addEventListener("click", mark_task_done)
+}
+
+function mark_task_done(event){
+  var current_element = this;
+  var this_link = this.getAttribute("href");
+
+  var my_request = new XMLHttpRequest();
+
+  my_request.open("get", this_link);
+
+  my_request.addEventListener("load", function(event){
+    var parent = current_element.parentElement;
+    console.log(parent);
+    parent.className = "task finished";
+    parent.removeChild(current_element);
+  })
+
+  event.preventDefault();
+
+  my_request.send();
 }
